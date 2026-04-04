ee446c824fbd426f761fdac0769e2889c9ae9482
David Groß <d.g1r2o3s4s@gmail.com>
2026-04-04 18:21:29 +0200

    added team_comments.js with array teamComments

---
+++ scripts/team_comments.js
@@ -0,0 +1,23 @@
+const teamComments = [
+    {
+        name: "V. Schuster",
+        role: "Team Partner",
+        comment: "David really kept the team together with his great organization and clear communication. We wouldn't have got this far without his commitment.",
+        imgsrc: "./assets/img/v-schuster.png",
+        class : "schuster-img"
+    },
+    {
+        name: "E. Eichinger",
+        role: "Team Partner",
+        comment: "David was a top team colleague at DA. His positive commitment and willingness to take on responsibility made a significant contribution to us achieving our goals.",
+        imgsrc: "./assets/img/e-eichinger.png",
+        class : "eichinger-img"
+    }, 
+    {
+        name: "I. Nuber",
+        role: "Frontend Engineer",
+        comment: "It was a great pleasure to work with David. He knows how to push and encourage team members to present the best work possible, always adding something to brainstorm. Regarding the well-being of group members, he was always present and available to listen and help others, with a great sense of humor as well.",
+        imgsrc: "./assets/img/i-nuber.png",
+        class : "nuber-img"
+    }
+]
