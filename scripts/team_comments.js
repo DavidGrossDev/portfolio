872b3e86f22b346f59f292f94f010ba02dd10e67
David Groß <d.g1r2o3s4s@gmail.com>
2026-04-11 18:35:00 +0200

    edit team_comments.js (added commentDe)

--- scripts/team_comments.js
+++ scripts/team_comments.js
@@ -3,6 +3,7 @@ const teamComments = [
         name: "V. Schuster",
         role: "Team Partner",
         comment: "David really kept the team together with his great organization and clear communication. We wouldn't have got this far without his commitment.",
+        commentDe:"David hat das Team mit seiner starken Organisation und klaren Kommunikation maßgeblich zusammengehalten. Ohne seinen Einsatz wären wir nicht so weit gekommen.",
         imgsrc: "./assets/img/v-schuster.png",
         class : "schuster-img"
     },
@@ -10,6 +11,7 @@ const teamComments = [
         name: "E. Eichinger",
         role: "Team Partner",
         comment: "David was a top team colleague at DA. His positive commitment and willingness to take on responsibility made a significant contribution to us achieving our goals.",
+        commentDe:"David war ein sehr geschätzter Teamkollege bei DA. Sein positives Engagement und seine Bereitschaft, Verantwortung zu übernehmen, haben wesentlich dazu beigetragen, dass wir unsere Ziele erreicht haben.",
         imgsrc: "./assets/img/e-eichinger.png",
         class : "eichinger-img"
     }, 
@@ -17,6 +19,7 @@ const teamComments = [
         name: "I. Nuber",
         role: "Frontend Engineer",
         comment: "It was a great pleasure to work with David. He knows how to push and encourage team members to present the best work possible, always adding something to brainstorm. Regarding the well-being of group members, he was always present and available to listen and help others, with a great sense of humor as well.",
+        commentDe: "Es war eine große Freude, mit David zusammenzuarbeiten. Er weiß, wie man Teammitglieder motiviert und dazu anspornt, ihr bestes Arbeitsergebnis zu liefern, und bringt sich stets aktiv in Brainstorming-Prozesse ein. Auch für das Wohlbefinden des Teams war er immer ansprechbar, unterstützend und hilfsbereit und überzeugte zudem mit einem großartigen Sinn für Humor.",
         imgsrc: "./assets/img/i-nuber.png",
         class : "nuber-img"
     }
