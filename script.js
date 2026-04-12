89fb94dd98635e3f36b1c3398f4ebdac066bbe69
David Groß <d.g1r2o3s4s@gmail.com>
2026-04-12 16:51:08 +0200

    sorted scripts(templates in templates.js)

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const headerLinks = document.querySelectorAll('.landing-section-header-links-highlight-div a');
 const commentsAreaRef = document.getElementById('comments_area');
 const commentMarks = document.querySelectorAll('.comment-mark');
-
-
+const commentsLeftArrowBtnRef = document.getElementById('comments_left_arrow');
+const commentsRightArrowBtnRef = document.getElementById('comments_right_arrow');
 let commentCounter = 0;
-
 
 headerLinks.forEach(link => {
     link.addEventListener("click", () => {
@@ -16,57 +15,35 @@ headerLinks.forEach(link => {
     });
 });
 
-function init() {
-    let currentComment ="";
-    if(langDe) {
-       currentComment = teamComments[commentCounter].commentDe;
-    } else {
-        currentComment = teamComments[commentCounter].comment;
-    }
-    commentsAreaRef.innerHTML = `<section class="comment-container">
-                    <svg width="97" height="69" viewBox="0 0 97 69" fill="none">
-                        <path d="M30.6 68.1H0L28.8 0H48.3L30.6 68.1ZM79.2 68.1H48.6L77.4 0H96.9L79.2 68.1Z"
-                            fill="#70E61C" />
-                    </svg>
-                    <p>
-                       ${currentComment}
-                    </p>
-                    <div class="comment-footer">
-                        <svg width="405" height="2" viewBox="0 0 405 2" fill="none">
-                            <path d="M404.5 1H0" stroke="#1CDAE6" stroke-width="2" />
-                        </svg>
-                        <p>${teamComments[commentCounter].name} - ${teamComments[commentCounter].role}</p>
-                    </div>
-                </section>
-                <section class="comment-img-container">
-                    <div class="img-circle">
-                        <img class="${teamComments[commentCounter].class}" src="${teamComments[commentCounter].imgsrc}" alt="">
-                    </div>
-                    <svg width="102" height="4" viewBox="0 0 102 4" fill="none">
-                        <path
-                            d="M2 0C0.89543 0 0 0.89543 0 2C0 3.10457 0.89543 4 2 4V0ZM102 4H104V0H102V4ZM2 4H102V0H2V4Z"
-                            fill="#70E61C" />
-                    </svg>
-                </section>`;
-    addCommentMarkHighlight();
-}
-
-function reduceCommentCounter() {
+commentsLeftArrowBtnRef?.addEventListener('click', () => {
     commentCounter--;
     if (commentCounter < 0) {
         commentCounter = 2;
     }
     removeCommentMarkHighlight();
-    init();
-}
+    init(currentLang);
+});
 
-function increaseCommentCounter() {
+commentsRightArrowBtnRef?.addEventListener('click', () => {
     commentCounter++;
     if (commentCounter >= 3) {
         commentCounter = 0;
     }
     removeCommentMarkHighlight();
-    init();
+    init(currentLang);
+});
+
+function init() {
+    let currentComment = "";
+    let currentCheckboxTxt = "";
+    let lang = setCurrentLanguage();
+    
+    setLanguage(lang);
+    currentComment = teamComments[commentCounter][`comment_${lang}`];
+    currentCheckboxTxt = translationTemplatePriPol[lang];
+    commentsAreaRef.innerHTML = renderComment(currentComment);
+    addCommentMarkHighlight();
+    checkboxProPolContainerRef.innerHTML = currentCheckboxTxt;
 }
 
 function removeCommentMarkHighlight() {
---
+++ scripts/templates.js
@@ -0,0 +1,104 @@
+const translationTemplatePriPol = {
+    en: `<p>i've read the <a class="privacy-policy-link" href="">privacy policy</a> and agree to the processing of my data as outlined.</p>`,
+    de: `<p>Ich habe die <a class="privacy-policy-link" href="">Datenschutzerklärung</a> gelesen und stimme zu.</p>`
+}
+
+const translationtemplatesLegalNotice = {
+    en: {
+        acceptance: `By accessing and using <span>Portfolio</span> (Product), you acknowledge and agree to the following
+                    terms and conditions, and any policies, guidelines, or amendments thereto that may be presented to
+                    you from time to time. We, the listed students, may update or change the terms and conditions from
+                    time to time without notice.`,
+        scope: `<span>Portfolio</span> has been developed as part of a student group project in a web development
+                    bootcamp at the
+                    <span>Developer Akademie GmbH</span>. It has an educational purpose and is not intended for
+                    extensive personal &
+                    business usage. As such, we cannot guarantee consistent availability, reliability, accuracy, or any
+                    other aspect of quality regarding this Product.
+                    <br><br>
+                    The design of <span>Portfolio</span> is owned by the <span>Developer Akademie GmbH</span>.
+                    Unauthorized use, reproduction,
+                    modification, distribution, or replication of the design is strictly prohibited.`,
+        proprietary: `Aside from the design owned by <span>Developer Akademie GmbH</span>, we, the listed students, retain
+                    all
+                    proprietary rights in <span>Portfolio</span>, including any associated copyrighted material,
+                    trademarks, and
+                    other proprietary information.`,
+        use: `<span>Portfolio</span> is intended to be used for lawful purposes only, in accordance with all
+                    applicable laws
+                    and regulations. Any use of <span>Portfolio</span> for illegal activities, or to harass, harm,
+                    threaten, or
+                    intimidate another person, is strictly prohibited. You are solely responsible for your interactions
+                    with other users of <span>Portfolio</span>.`,
+        disclaimer: `<span>Portfolio</span> is provided "as is" without warranty of any kind, whether express or implied,
+                    including
+                    but not limited to the implied warranties of merchantability, fitness for a particular purpose, and
+                    non-infringement. In no event will we, the listed students, or the <span>Developer Akademie</span>,
+                    be liable for
+                    any direct, indirect, incidental, special, consequential or exemplary damages, including but not
+                    limited to, damages for loss of profits, goodwill, use, data, or other intangible losses, even if we
+                    have been advised of the possibility of such damages, arising out of or in connection with the use
+                    or performance of <span>Portfolio</span>.`,
+        indemnity: `You agree to indemnify, defend and hold harmless us, the listed students, the Developer Akademie,
+                    and our affiliates, partners, officers, directors, agents, and employees, from and against any
+                    claim, demand, loss, damage, cost, or liability (including reasonable legal fees) arising out of or
+                    relating to your use of Portfolio and/or your breach of this Legal Notice.
+                    <br><br>
+                    For any questions or notices, please contact us at <span>info@developerakademie.com</span>.
+                    <br><br>
+                    Date: April 5, 2026`
+
+    },
+    de: {
+        acceptance: `Durch den Zugriff auf und die Nutzung von <span>Portfolio</span> (Produkt) erklären Sie sich mit den folgenden Nutzungsbedingungen sowie allen Richtlinien, Bestimmungen oder Änderungen, die Ihnen gegebenenfalls vorgelegt werden, einverstanden. Wir, die aufgeführten Studierenden, behalten uns das Recht vor, die Nutzungsbedingungen jederzeit ohne vorherige Ankündigung zu aktualisieren oder zu ändern.`,
+        scope: `Das <span>Portfolio</span> wurde im Rahmen eines studentischen Gruppenprojekts in einem Webentwicklungs-Bootcamp an der <span>Developer Akademie GmbH</span> entwickelt. Es dient ausschließlich Bildungszwecken und ist nicht für den umfangreichen privaten und
+                geschäftlichen Gebrauch bestimmt. Daher können wir keine ständige Verfügbarkeit, Zuverlässigkeit, Genauigkeit oder sonstige Qualitätsmerkmale dieses Produkts garantieren.
+                <br><br>
+                Das Design des <span>Portfolios</span> ist Eigentum der
+                <span>Developer Akademie GmbH</span>.
+                Unerlaubte Nutzung, Vervielfältigung,
+                Änderung, Verbreitung oder Reproduktion des Designs ist strengstens untersagt.`,
+        proprietary: `Abgesehen vom Design, das der Developer Akademie GmbH gehört, behalten wir, die aufgeführten Studierenden, alle Eigentumsrechte an <span>Portfolio</span>, einschließlich aller zugehörigen urheberrechtlich geschützten Materialien, Marken und sonstigen geschützten Informationen.`,
+        use: `Das <span>Portfolio</span> darf ausschließlich für rechtmäßige Zwecke und in Übereinstimmung mit allen geltenden Gesetzen und Vorschriften verwendet werden. Jegliche Nutzung des <span>Portfolio</span> für illegale Aktivitäten oder zur Belästigung, Schädigung, Bedrohung oder Einschüchterung anderer Personen ist strengstens untersagt. Sie tragen die alleinige Verantwortung für Ihre Interaktionen mit anderen Nutzern des <span>Portfolio</span>.`,
+        disclaimer: `Das <span>Portfolio</span> wird ohne jegliche Gewährleistung, weder ausdrücklich noch stillschweigend, bereitgestellt.
+                Dies schließt insbesondere die Gewährleistung der Handelsüblichkeit, der Eignung für einen bestimmten Zweck und der Nichtverletzung von Rechten Dritter aus.
+                Wir, die aufgeführten Studierenden und die <span>Developer Akademie</span> haften in keinem Fall für
+                direkte, indirekte, zufällige, besondere, Folgeschäden oder Strafschadenersatz, einschließlich, aber nicht beschränkt auf
+                Schäden aus entgangenem Gewinn, Verlust des Firmenwerts, Nutzungsausfall, Datenverlust oder sonstige immaterielle Verluste, selbst wenn wir auf die Möglichkeit solcher Schäden hingewiesen wurden, die aus der Nutzung oder der Leistung des <span>Portfolios</span> entstehen oder damit in Zusammenhang stehen.`,
+        indemnity: `Sie verpflichten sich, uns, die aufgeführten Studierenden, die Developer Akademie
+                sowie unsere verbundenen Unternehmen, Partner, Führungskräfte, Direktoren, Vertreter und Mitarbeiter von jeglichen
+                Ansprüchen, Forderungen, Verlusten, Schäden, Kosten oder Haftungen (einschließlich angemessener Anwaltskosten) freizustellen, zu verteidigen und schadlos zu halten, die sich aus Ihrer Nutzung des Portfolios und/oder Ihrem Verstoß gegen diese rechtlichen Hinweise ergeben.
+                <br><br>
+                Bei Fragen oder Anmerkungen kontaktieren Sie uns bitte unter <span>info@developerakademie.com</span>.
+                <br><br>
+                Datum: 5. April 2026`
+    }
+}
+
+function renderComment(currentComment) {
+    return `<section class="comment-container">
+                    <svg width="97" height="69" viewBox="0 0 97 69" fill="none">
+                        <path d="M30.6 68.1H0L28.8 0H48.3L30.6 68.1ZM79.2 68.1H48.6L77.4 0H96.9L79.2 68.1Z"
+                            fill="#70E61C" />
+                    </svg>
+                    <p>
+                       ${currentComment}
+                    </p>
+                    <div class="comment-footer">
+                        <svg width="405" height="2" viewBox="0 0 405 2" fill="none">
+                            <path d="M404.5 1H0" stroke="#1CDAE6" stroke-width="2" />
+                        </svg>
+                        <p>${teamComments[commentCounter].name} - ${teamComments[commentCounter].role}</p>
+                    </div>
+                </section>
+                <section class="comment-img-container">
+                    <div class="img-circle">
+                        <img class="${teamComments[commentCounter].class}" src="${teamComments[commentCounter].imgsrc}" alt="">
+                    </div>
+                    <svg width="102" height="4" viewBox="0 0 102 4" fill="none">
+                        <path
+                            d="M2 0C0.89543 0 0 0.89543 0 2C0 3.10457 0.89543 4 2 4V0ZM102 4H104V0H102V4ZM2 4H102V0H2V4Z"
+                            fill="#70E61C" />
+                    </svg>
+                </section>`;
+}
