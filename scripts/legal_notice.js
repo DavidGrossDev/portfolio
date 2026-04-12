6a5cfc5a591f7a5d44f9f044e16074fa603c7736
David Groß <d.g1r2o3s4s@gmail.com>
2026-04-12 16:51:43 +0200

    added legal_notice.js and added functionality

---
+++ scripts/legal_notice.js
@@ -0,0 +1,17 @@
+const ids = ['acceptance', 'scope', 'proprietary', 'use', 'disclaimer', 'indemnity'];
+
+const sectionLegNotArray = ids.map(id =>
+    document.getElementById(`legal_notice_${id}`)
+);
+
+function legalNoticeInit() {
+    let lang = setCurrentLanguage();
+    setLanguage(lang);
+    let keyArray = Object.keys(translationtemplatesLegalNotice[lang]);
+    for (let index = 0; index < sectionLegNotArray.length; index++) {
+        sectionLegNotArray[index].innerHTML = translationtemplatesLegalNotice[lang][keyArray[index]];
+    }
+}
+
+
+
